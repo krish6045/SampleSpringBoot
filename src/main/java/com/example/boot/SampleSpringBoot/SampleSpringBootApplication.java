4b1c0e2957d8036d6548572f405acbea4b66e10c
krish6045 <dev.sasikrishna@gmail.com>
2018-12-08 23:04:01 +0530

    Added project files

---
+++ src/main/java/com/example/boot/SampleSpringBoot/SampleSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.example.boot.SampleSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringBootApplication.class, args);
+	}
+}
